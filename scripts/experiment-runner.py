d9d55528f48067f763c542a3508783279486a9af
Derrick McKee <derrick.mckee@gmail.com>
2022-01-24 13:17:03 -0500

    initial commit of experiment runner

---
+++ scripts/experiment-runner.py
@@ -0,0 +1,145 @@
+import yaml
+import click
+import os
+import subprocess
+import torch
+import pickle
+
+import asm2vec
+
+
+class Training:
+    def __init__(self):
+        self.model_path = None
+        self.binary_path = None
+        self.function_path = None
+        self.results = list()
+
+    def add_result(self, training_func, test_func, similarity):
+        self.results.append((training_func, test_func, similarity))
+
+    def __hash__(self):
+        return hash(self.model_path, self.binary_path, self.function_path)
+
+    def __eq__(self, other):
+        return self.binary_path == other.binary_path
+
+
+class TestBinary:
+    def __init__(self):
+        self.binary_path = None
+        self.function_path = None
+
+    def __hash__(self):
+        return hash(self.binary_path, self.function_path)
+
+    def __eq__(self, other):
+        return self.binary_path == other.binary_path
+
+
+def cosine_similarity(v1, v2):
+    return (v1 @ v2 / (v1.norm() * v2.norm())).item()
+
+
+def generate_assembly(binary: str, output_dir: str):
+    print(f"Generating assembly for {binary}")
+    bin2asm_path = os.path.join(os.path.dirname(__file__), 'bin2asm.py')
+    cmd = ['python3', bin2asm_path, '-i', binary, '-o', output_dir]
+    subprocess.run(cmd, check=True)
+
+
+def generate_model(function_path: str, model_path: str):
+    print(f"Generating model {model_path}")
+    train_path = os.path.join(os.path.dirname(__file__), 'train.py')
+    cmd = ['python3', train_path, '-i', function_path, '-o', model_path, '--epochs', 100]
+    subprocess.run(cmd, check=True)
+
+
+def compare_functions(func1, func2, mpath):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    epochs = 10
+    lr = 0.02
+
+    # load model, tokens
+    model, tokens = asm2vec.utils.load_model(mpath, device=device)
+    functions, tokens_new = asm2vec.utils.load_data([func1, func2])
+    tokens.update(tokens_new)
+    model.update(2, tokens.size())
+    model = model.to(device)
+
+    # train function embedding
+    model = asm2vec.utils.train(
+        functions,
+        tokens,
+        model=model,
+        epochs=epochs,
+        device=device,
+        mode='test',
+        learning_rate=lr
+    )
+
+    # compare 2 function vectors
+    v1, v2 = model.to('cpu').embeddings_f(torch.tensor([0, 1]))
+    return cosine_similarity(v1, v2)
+
+
+@click.command()
+@click.option('-i', '--input', 'ipath', help='input yaml', required=True)
+@click.option('-o', '--output', 'opath', help='output object', required=True)
+def main(ipath, opath):
+    with open(ipath, 'r') as f:
+        experiment_yaml = yaml.safe_load(f)
+
+    trainings = list()
+    for yaml_training in experiment_yaml['training']:
+        dest_path = yaml_training['dest_path']
+        training_name = yaml_training['name']
+        root_name = yaml_training['root_dir']
+        for binary in yaml_training['binaries']:
+            training = Training()
+            training.model_path = os.path.join(dest_path, training_name, binary,
+                                               "model.pt")
+            training.binary_path = os.path.join(root_name, binary)
+            training.function_path = os.path.join(os.path.dirname(training.model_path),
+                                                  'asm')
+            if not os.path.exists(training.binary_path):
+                raise FileNotFoundError(training.binary_path)
+            trainings.append(training)
+
+    tests = list();
+    for yaml_test in experiment_yaml['experiments']:
+        test_name = yaml_test['name']
+        root_name = yaml_test['root_dir']
+        dest_path = yaml_test['dest_path']
+        tests[test_name] = list()
+        for binary in yaml_test['binaries']:
+            test_bin = TestBinary()
+            test_bin.binary_path = os.path.join(root_name, binary)
+            test_bin.function_path = os.path.join(dest_path, test_name, binary, 'asm')
+
+            if not os.path.exists(test_bin.binary_path):
+                raise FileNotFoundError(test_bin.binary_path)
+
+            tests.append(test_bin)
+
+    for training in trainings:
+        generate_assembly(training.binary_path, training.function_path)
+        generate_model(training.function_path, training.model_path)
+    for test in tests:
+        generate_assembly(test.binary_path, test.function_path)
+
+    for training in trainings:
+        for test in tests:
+            print(f"Comparing {training.binary_path} with {test.binary_path}")
+            for training_root, _, training_func in os.walk(training.function_path):
+                for test_root, _, test_func in os.walk(test.function_path):
+                    similarity = compare_functions(training_func, test_func,
+                                                   training.model_path)
+                    training.add_result(os.path.join(training_root, training_func),
+                                        os.path.join(test_root, test_func), similarity)
+    with open(opath, 'wb') as f:
+        pickle.dump(trainings, f)
+
+
+if __name__ == '__main__':
+    main()
